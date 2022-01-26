bf77fc508e2b6a939ee724cf9c9cc40643667974
Ekin Celebi <ekincelebi@anadolusigorta.com.tr>
2022-01-26 11:04:07 +0100

    Dialogs & Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-], function (Controller, MessageToast) {
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         
@@ -13,6 +14,19 @@ sap.ui.define([
 
             // show a native JavaScript alert
             MessageToast.show(sMsg);
-        }
+        },
+
+        onOpenDialog : function () {
+
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				});
+			} 
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		}
     });
 });
